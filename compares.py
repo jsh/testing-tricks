fcc4eafce6a9e43d91f32da72982f3ce5c6c7947
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2025-05-21 13:30:55 -0600

    Add comparison functions.

---
+++ compares.py
@@ -0,0 +1,23 @@
+def are_equal(a, b):
+    """Return True if a and b are equal, else False."""
+    return a == b
+
+def are_not_equal(a, b):
+    """Return True if a and b are not equal, else False."""
+    return a != b
+
+def is_greater(a, b):
+    """Return True if a is greater than b, else False."""
+    return a > b
+
+def is_less(a, b):
+    """Return True if a is less than b, else False."""
+    return a < b
+
+def is_greater_or_equal(a, b):
+    """Return True if a is greater than or equal to b, else False."""
+    return a >= b
+
+def is_less_or_equal(a, b):
+    """Return True if a is less than or equal to b, else False."""
+    return a <= b
